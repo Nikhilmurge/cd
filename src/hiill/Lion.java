63f8c851d27ab92ac19770546a923f5677143eeb
sachin <nmurge123@gmail.com>
2024-08-16 21:07:32 +0530

    hello

---
+++ src/hiill/Lion.java
@@ -0,0 +1,23 @@
+package hiill;
+
+public class Lion {
+
+	public static void main(String[] args) {
+		System.out.println("me");
+	}
+
+	public void m2() {
+		System.out.println("me");
+	}
+
+	public void m3() {
+		System.out.println("me");
+	}
+
+	public void m4() {
+		{
+			System.out.println("me");
+		}
+
+	}
+}
